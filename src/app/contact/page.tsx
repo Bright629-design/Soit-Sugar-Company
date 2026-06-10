8adf6403feb73e0ee457748d98e875d8788b9c8b
Emilio.Dev <cheruiyotemicky@gmail.com>
2026-06-10 21:51:09 +0300

    Update page.tsx

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -81,14 +81,14 @@ export default function ContactPage() {
                                     <label htmlFor="name"
                                         className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">Full
                                         Name</label>
-                                    <input id="name" placeholder="John Doe"
+                                    <input id="name" placeholder="Emilio Cheruiyot"
                                         className="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50" />
                                 </div>
                                 <div className="space-y-2">
                                     <label htmlFor="email"
                                         className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">Email
                                         Address</label>
-                                    <input id="email" type="email" placeholder="john.doe@example.com"
+                                    <input id="email" type="email" placeholder="emiliocheruiyot@gmail.com"
                                         className="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50" />
                                 </div>
                             </div>
